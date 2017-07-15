c528615009c425c5671293b36c7c34b46d9917c8
yu.wang <yu.wang1@fda.hhs.gov>
2017-07-15 12:42:26 -0400

    Add "my_subtract" in "compute.py".
    Add "test_my_subtract1" and "test_my_subtract2" in "tests/test_compute.py".

--- sprint_tutorial/compute.py
+++ sprint_tutorial/compute.py
@@ -6,3 +6,9 @@ def my_sum(x, y):
     """ Compute the sum of 2 numbers
     """
     return 2*(x + y)
+
+def my_subtract(x, y):
+    """ Subtract the second number from the first number
+    """
+    return (x - y)
+    
--- sprint_tutorial/tests/test_compute.py
+++ sprint_tutorial/tests/test_compute.py
@@ -3,6 +3,7 @@
 from nose.tools import assert_equal
 
 from sprint_tutorial.compute import my_sum
+from sprint_tutorial.compute import my_subtract
 
 
 def test_my_sum1():
@@ -11,3 +12,9 @@ def test_my_sum1():
 
 def test_my_sum2():
     assert_equal(my_sum(1, -1), 0)
+
+def test_my_subtract1():
+    assert_equal(my_subtract(2, 1), 1)
+
+def test_my_subtract2():
+    assert_equal(my_subtract(5, 3), 2)
